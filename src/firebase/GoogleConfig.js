6f7b8d9638049cb21a2d5f7f9d5120550d5fb0aa
Saurabh7763 <saurabhsingh797763@gmail.com>
2026-02-18 22:42:10 +0530

    Delete src/firebase/GoogleConfig.js for safety purpose

--- src/firebase/GoogleConfig.js
+++
@@ -1,6 +0,0 @@
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-
-GoogleSignin.configure({
-webClientId: '983359654145-ahd6ri7v9q9tm9mfq2gqc0rhp10p2oft.apps.googleusercontent.com',
-offlineAccess: true,
-});
