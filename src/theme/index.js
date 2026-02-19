a140fc1acf8d35e9bf9f8aceed78845bbde00426
Saurabh Singh <saurabhsingh797763@gmail.com>
2026-02-20 00:30:15 +0530

    added app icon

--- src/theme/index.js
+++ src/theme/index.js
@@ -9,3 +9,21 @@ export const categoryBG = {
   entertainment: "#FEF3C7", 
   other: "#E2E8F0"          
 };
+
+// export const lightTheme = {
+//   background: '#F1F5F9',
+//   card: '#ffffff',
+//   text: '#1E293B', 
+//   subText: '#64748B',
+//   button: '#22C55E',
+//   danger: '#EF4444',
+// };
+
+// export const darkTheme = {
+//   background: '#0F172A',
+//   card: '#1E293B',
+//   text: '#F1F5F9',
+//   subText: '#94A3B8',
+//   button: '#22C55E',
+//   danger: '#EF4444',
+// };
